0179dff4e4d9c0b64dfdd2d479e49575e59688b3
MetehanGuven <101170727+MetehanGuven@users.noreply.github.com>
2022-11-23 21:38:06 +0300

    First episode is done.

---
+++ Zombie.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	system("color 9");
+	int choice;
+	setlocale(LC_ALL, "Turkish");
+	cout << " ----- HOÞGELDÝNÝZ SENA HANIM AÞKIM BÝTANEM ----- " << endl;
+	cout << " Yeni uyandýn ve twitter de haberlere bakmak istedin. Þaþkýnlýkla haberleri okuyorsun, çünkü zombi salgýný baþlamýþ." << endl;
+	cout << " Ne yapacaðýný bilmiyorsun, bir yandan da mutlu olmadýn deðil. Çünkü TWD izlemiþsin ve böyle bir olay olmasýný istemiyor deðildin zaten. Heyecanlýsýn. " << endl;
+	cout << " Ýlk olarak ne yapacaksýn ? " << endl;
+	cout << " 1) Yataktan kalkýp kahvaltý yapmak. " << endl;
+	cout << " 2) Meteni uyandýrýp hazýrlýklara baþlamak. " << endl;
+	cout << " 3) Aileni arayýp otobüs bileti almak. " << endl;
+	cout << " Lütfen birini seç: ";
+	cin >> choice;
+	switch (choice) {
+	case 1: {
+		cout << " Saçma saçma iþ yapma dünya elden gidiyor !! ";
+		break;
+	}
+	case 2: {
+		cout << " Uyandýrdýn ve hazýrlýklara baþladýnýz. " << endl;
+		cout << " Þimdi ne yapcaksýnýz peki ? " << endl;
+		cout << " 1) Erzak toplamaya çýkmak. " << endl;
+		cout << " 2) Silah bulmaya çýkmak. " << endl;
+		cout << " 3) Arkadaþlarýnla parkta buluþmak. " << endl;
+		cout << "Lütfen birini seç : ";
+		cin >> choice;
+		switch (choice)
+		{
+		case 1: {
+			cout << " Erzak toplamak için Espark a gittiniz ve içerden sesler duyuyosunuz." << endl;
+			cout << " Ýçerdekiler insan da olabilir zombi de. Bu riske girecek misiniz? " << endl;
+			cout << " 1) Gir" << endl;
+			cout << " 2) Kaç" << endl;
+			cout << "Lütfen birini seç : ";
+			cin >> choice;
+			switch (choice)
+			{
+			case 1:
+			{
+				cout << " Þanslýsýnýz çünkü içerde Mehmet ile karþýlaþtýnýz. " << endl;
+				cout << " O akýllý da hemen erzak toplamaya gitmiþ ve sizden çaldýðý makasý da yanýnda getirmiþ. " << endl;
+				cout << " Tam mutlu oldunuz derken arkanýzdan zombi sesi geliyor ve sen çok korktun. " << endl;
+				cout << " Zombi seni ýsýrmak için hamle yapmýþken çok sevdiðin sevgilin zombinin önüne atlýyor ve hayatýný kurtarýyor. " << endl;
+				cout << " Bir hamle yapýp zombinin kafasýný kopartýyor. Sen tekrardan aþýk oluyorsun " << endl;
+				cout << " Bu olaydan hemen sonra migrosa giriyorsunuz ve belli bir süre orada yaþýyorsunuz ayný zamanda haberleri takip ediyorsunuz. " << endl;
+				cout << " Bir süre sonra internet de çekmemeye baþlýyor, ailenizden haber almamaya baþladýnýz ve iyice korkmaya baþlýyorsunuz. " << endl;
+				cout << " Ýnternet kesilmeden hemen önce Ankara da bir sýðýnak olduðu haberini okuyorsun. " << endl;
+				cout << " 1) Sýðýnaða doðru yola çýkýn" << endl;
+				cout << " 2) Habere aldýrýþ etmeyip ailenizle iletiþim kurabilmek için telefonun çekeceði bir yer bulmaya çalýþýn. " << endl;
+				cout << " Lütfen birini seç: ";
+				cin >> choice;
+				switch (choice)
+				{
+				case 1:
+				{
+					cout << " Ankaraya gitmek için araba bulmaya baþladýnýz fakat caddeye çýkmaya korkuyorsunuz." << endl;
+					cout << " Zeki sevgilinin aklýna otopark geliyor ve otoparka doðru gitmeye baþladýnýz. Migrostan çýkmadan önce yanýnýza balta, býçak vs. gibi aletler de yanýnýza aldýnýz tabi :) " << endl;
+					cout << " Otoparka vardýnýz ve çok þanslýsýnýz çünkü hiç zombi yok. " << endl;
+					cout << " Sen tecrübeli olduðun için düz kontak yapýp bir arabayý çalýþtýrdýnýz ve yola çýkmaya baþladýnýz. " << endl;
+					cout << " Arabayý kim kullansýn ? " << endl;
+					cout << " 1) Mehmet" << endl;
+					cout << " 2) Mete" << endl;
+					cout << " Kendini þýklarda arama çünkü tecrübeli deðilsin henüz. Oyy fýstýðým merak etme öðreticem ben <3 <3 " << endl;
+					cout << "Lütfen birini seç : ";
+					cin >> choice;
+					switch (choice)
+					{
+					case 1: 
+					{
+						cout << " !! ÖLDÜNÜZ !! ";
+						break;
+					}
+					case 2:
+					{
+						cout << " Doðru seçim. " << endl;
+						cout << " Ankaraya vardýnýz ve sýðýnaða girdiniz. " << endl;
+						cout << " Askerler tarafýndan kontrolleriniz yapýlýyor. " << endl;
+						cout << " Kalan olaylar diðer bölümde .... ";
+					}
+					}
+					}
+					}
+
+				}
+				}
+
+			}
+			}
+		}
+		
+	
+}
+return 0;
+}
+	
